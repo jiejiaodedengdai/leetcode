16c1bb179b3260e8e65f76c40df9d8b9f9a6c230
jiejiaodedengdai <92498344+jiejiaodedengdai@users.noreply.github.com>
2025-09-25 07:17:15 +0000

    爬楼梯最小代价dp

---
+++ leetcode/leetcode_746.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+
+
+        // dp[i] 表示爬到第i个台阶的最小花费
+
+        // dp[i] = min( dp[i-1]+ cost[i-1] , dp[i-2]+ cost[i-2]);
+        int dp[] = new int[ cost.length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for(int i = 2; i <= cost.length ; i++){
+            dp[i] = Math.min(dp[i-1]+ cost[i-1] , dp[i-2]+ cost[i-2] );
+        }
+
+        return dp[cost.length ];
+    }
+}
